92115168fad2ac672af7ff96a941447472f3ea5c
athibanvasanth <athibanvasanth@gmail.com>
2026-08-11 14:04:14 +0530

    feat: add inline SVG favicon to daily digest page

--- generate_digest.py
+++ generate_digest.py
@@ -569,6 +569,7 @@ def build_html(digest_content, newsletters, rss_articles, tldr_articles):
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Daily Digest \u2014 {date_str}</title>
+    <link rel="icon" href="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'%3E%3Crect width='16' height='16' fill='%23000'/%3E%3Cpath d='M4 3a1 1 0 0 1 1 1v8a1 1 0 0 1-2 0V4a1 1 0 0 1 1-1zm7 1.5c0-1-1.5-1.5-2.5-1.5a1 1 0 1 0 0 2c.5 0 1.5.5 1.5 1.5v3c0 1-1 1.5-1.5 1.5a1 1 0 1 0 0 2c1 0 2.5-.5 2.5-1.5v-7z' fill='%23e8734a'/%3E%3C/svg%3E">
     <style>
         * {{ margin: 0; padding: 0; box-sizing: border-box; }}
         body {{
